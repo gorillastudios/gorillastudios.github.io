f3e23933864092afbcbb4ca2ea751d1008269d86
Chris Peak <cpeak@8thlight.com>
2015-07-17 20:00:24 -0500

    Randomized index page backgrounds

---
+++ _site/js/index-backgrounds.js
@@ -0,0 +1,18 @@
+var backgrounds = [
+  "/images/chicago/inky-lake2.jpg",
+  "/images/london/london-eye-2015.jpg",
+  "/images/london/shard-streaks.jpg",
+  "/images/new-england/watch-hill-3.jpg",
+  "/images/p52/25.jpg",
+  "/images/portfolio/dance.jpg",
+  "/images/portfolio/masons-dock.jpg",
+  "/images/bm/12.jpg",
+  "/images/bm/5.jpg",
+  "/images/bm/6.jpg",
+  "/images/chicago/sunrays.jpg",
+  "/images/catalog/0K6A7508.jpg",
+  "/images/catalog/0K6A7506.jpg",
+  "/images/catalog/0K6A2937.jpg",
+  "/images/union/28.jpg",
+  "/images/portfolio/archways.jpg"
+  ]
