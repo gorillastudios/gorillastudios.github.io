d31f3fb7678bba1d2a7b940d5de3807ba21d50ab
Chris Peak <cpeak@8thlight.com>
2015-08-10 08:50:10 -0500

    Updated photos, drafted blog post

--- js/index-backgrounds.js
+++ js/index-backgrounds.js
@@ -1,17 +1,25 @@
 var backgrounds = [
+  "/images/bfbc/0K6A4261.jpg",
+  "/images/bfbc/0K6A4780.jpg",
+  "/images/bfbc/0K6A4708.jpg",
+  "/images/bfbc/0K6A4915.jpg",
+  "/images/london/shard-streaks.jpg",
+  "/images/p52/25.jpg",
+  "/images/bm/12.jpg",
+  "/images/bm/6.jpg",
+  "/images/catalog/0K6A7506.jpg",
+  ]
+
+/*
+  "/images/union/28.jpg",
+  "/images/catalog/0K6A2937.jpg",
+  "/images/new-england/watch-hill-3.jpg",
+  "/images/portfolio/masons-dock.jpg",
+  "/images/portfolio/dance.jpg",
+  "/images/portfolio/archways.jpg"
   "/images/chicago/inky-lake2.jpg",
   "/images/london/london-eye-2015.jpg",
-  "/images/london/shard-streaks.jpg",
-  "/images/new-england/watch-hill-3.jpg",
-  "/images/p52/25.jpg",
-  "/images/portfolio/dance.jpg",
-  "/images/portfolio/masons-dock.jpg",
-  "/images/bm/12.jpg",
   "/images/bm/5.jpg",
-  "/images/bm/6.jpg",
   "/images/catalog/0K6A7508.jpg",
-  "/images/catalog/0K6A7506.jpg",
-  "/images/catalog/0K6A2937.jpg",
-  "/images/union/28.jpg",
-  "/images/portfolio/archways.jpg"
-  ]
+
+*/
